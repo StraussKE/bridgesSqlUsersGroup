32e843435239efb8d35cdc551b42649ff4531153
Katie Strauss <straussk@my.lanecc.edu>
2020-03-11 13:39:49 -0700

    scaffolded meeting, speaker, sponsor views and controllers

---
+++ bridgesSqlUsersGroup/Controllers/MeetingController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using bridgesSqlUsersGroup.Models;
+
+namespace bridgesSqlUsersGroup.Controllers
+{
+    public class MeetingController : Controller
+    {
+        private readonly AppIdentityDbContext _context;
+
+        public MeetingController(AppIdentityDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Meeting
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Meetings.ToListAsync());
+        }
+
+        // GET: Meeting/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meeting = await _context.Meetings
+                .FirstOrDefaultAsync(m => m.MeetingId == id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+
+            return View(meeting);
+        }
+
+        // GET: Meeting/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Meeting/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MeetingId,MeetingDate,Location,Topic,Description")] Meeting meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                meeting.MeetingId = Guid.NewGuid();
+                _context.Add(meeting);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(meeting);
+        }
+
+        // GET: Meeting/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meeting = await _context.Meetings.FindAsync(id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+            return View(meeting);
+        }
+
+        // POST: Meeting/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("MeetingId,MeetingDate,Location,Topic,Description")] Meeting meeting)
+        {
+            if (id != meeting.MeetingId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(meeting);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MeetingExists(meeting.MeetingId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(meeting);
+        }
+
+        // GET: Meeting/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meeting = await _context.Meetings
+                .FirstOrDefaultAsync(m => m.MeetingId == id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+
+            return View(meeting);
+        }
+
+        // POST: Meeting/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var meeting = await _context.Meetings.FindAsync(id);
+            _context.Meetings.Remove(meeting);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MeetingExists(Guid id)
+        {
+            return _context.Meetings.Any(e => e.MeetingId == id);
+        }
+    }
+}
---
+++ bridgesSqlUsersGroup/Controllers/SponsorsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using bridgesSqlUsersGroup.Models;
+
+namespace bridgesSqlUsersGroup.Controllers
+{
+    public class SponsorsController : Controller
+    {
+        private readonly AppIdentityDbContext _context;
+
+        public SponsorsController(AppIdentityDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Sponsors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Sponsors.ToListAsync());
+        }
+
+        // GET: Sponsors/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sponsor = await _context.Sponsors
+                .FirstOrDefaultAsync(m => m.SponsorId == id);
+            if (sponsor == null)
+            {
+                return NotFound();
+            }
+
+            return View(sponsor);
+        }
+
+        // GET: Sponsors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sponsors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SponsorId,Name")] Sponsor sponsor)
+        {
+            if (ModelState.IsValid)
+            {
+                sponsor.SponsorId = Guid.NewGuid();
+                _context.Add(sponsor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sponsor);
+        }
+
+        // GET: Sponsors/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sponsor = await _context.Sponsors.FindAsync(id);
+            if (sponsor == null)
+            {
+                return NotFound();
+            }
+            return View(sponsor);
+        }
+
+        // POST: Sponsors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("SponsorId,Name")] Sponsor sponsor)
+        {
+            if (id != sponsor.SponsorId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sponsor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SponsorExists(sponsor.SponsorId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sponsor);
+        }
+
+        // GET: Sponsors/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sponsor = await _context.Sponsors
+                .FirstOrDefaultAsync(m => m.SponsorId == id);
+            if (sponsor == null)
+            {
+                return NotFound();
+            }
+
+            return View(sponsor);
+        }
+
+        // POST: Sponsors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var sponsor = await _context.Sponsors.FindAsync(id);
+            _context.Sponsors.Remove(sponsor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SponsorExists(Guid id)
+        {
+            return _context.Sponsors.Any(e => e.SponsorId == id);
+        }
+    }
+}
